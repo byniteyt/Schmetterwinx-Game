8dd5d8dc0a86d587ff136f7f757f564d70796df4
ByNiteProYT <asiermartincaballero900@gmail.com>
2026-02-03 23:08:09 +0100

    next enemy action

---
+++ Puzzle Dungeon/Assets/Scripts/Characters/Enemies/EnemyWarning.cs
@@ -0,0 +1,35 @@
+using TMPro;
+using UnityEngine;
+
+public class EnemyWarning : MonoBehaviour
+{
+    public static EnemyWarning instance;
+    [SerializeField] private GameObject text;
+    [SerializeField] private Vector2 offset;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (instance==null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        ShowEnemiesIntentions();
+    }
+    public void ShowEnemiesIntentions()
+    {
+        foreach (GameObject enemy in GameObject.FindGameObjectsWithTag("Enemy"))
+        {
+            var textObject = Instantiate(text, FindAnyObjectByType<Canvas>().transform);
+            Vector3 screenPos = Camera.main.WorldToScreenPoint(enemy.transform.position);
+            textObject.transform.position = screenPos + (Vector3)offset;
+            textObject.GetComponent<TextMeshProUGUI>().text =
+                (enemy.GetComponent<EnemyBasic>().intention[0] == 0 ? "Attack" : "Block") + ": " +
+                enemy.GetComponent<EnemyBasic>().power[0];
+        }
+
+    }
+}
