8822b284bd27dc336c7ac8f8367d936c48a79007
1vanX <82110900+1vanX@users.noreply.github.com>
2026-02-04 21:50:04 +0100

    Vida de enemigos y Créditos

--- Puzzle Dungeon/Assets/Scripts/Characters/Enemies/EnemyWarning.cs
+++ Puzzle Dungeon/Assets/Scripts/Characters/Enemies/EnemyWarning.cs
@@ -6,6 +6,9 @@ public class EnemyWarning : MonoBehaviour
     public static EnemyWarning instance;
     public GameObject text;
     [SerializeField] private Vector2 offset;
+    //Esto es pq no se me ocurrá un nombre para el vec estupido copilot callate ya de verdad dejame poner los comentarios como me sale de las narices AAAAAAAAAAAAA de verdad vaya mrda
+    //Ah, por cierto lo que hace es bajar el texto porque se quedaba metido en los modelos
+    public Vector3 b = new Vector3(0, -15, 0);
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -18,6 +21,7 @@ public class EnemyWarning : MonoBehaviour
             Destroy(gameObject);
         }
         ShowEnemiesIntentions();
+        ShowEnemiesHP();
     }
     public void ShowEnemiesIntentions()
     {
@@ -25,11 +29,21 @@ public class EnemyWarning : MonoBehaviour
         {
             var textObject = Instantiate(text, FindAnyObjectByType<Canvas>().transform);
             Vector3 screenPos = Camera.main.WorldToScreenPoint(enemy.transform.position);
-            textObject.transform.position = screenPos + (Vector3)offset;
+            textObject.transform.position = screenPos + (Vector3)offset+b;
             textObject.GetComponent<TextMeshProUGUI>().text =
                 (enemy.GetComponent<EnemyBasic>().intention[0] == 0 ? "Attack" : "Block") + ": " +
                 enemy.GetComponent<EnemyBasic>().power[0];
         }
 
+    }
+    public void ShowEnemiesHP() {
+        foreach (GameObject enemy in GameObject.FindGameObjectsWithTag("Enemy"))
+        {
+            var textObject = Instantiate(text, FindAnyObjectByType<Canvas>().transform);
+            Vector3 screenPos = Camera.main.WorldToScreenPoint(enemy.transform.position);
+            textObject.transform.position = screenPos + (Vector3)offset+ (Vector3)offset;
+            textObject.GetComponent<TextMeshProUGUI>().text =
+                ("HP: "+enemy.GetComponent<EnemyBasic>().hp);
+        }
     }
 }
