d54ddc69533187705d9880730006686420dd8b07
ByNiteProYT <asiermartincaballero900@gmail.com>
2026-02-07 23:47:39 +0100

    dialogues

--- Puzzle Dungeon/Assets/Scripts/UI/GeneralButtons.cs
+++ Puzzle Dungeon/Assets/Scripts/UI/GeneralButtons.cs
@@ -11,7 +11,7 @@ public class GeneralButtons : MonoBehaviour
     }
     public void StartGame()
     {
-        SceneManager.LoadScene("GameWorld");
+        SceneManager.LoadScene("InitialDialogue");
     }
     public void ReturnToMainMenu()
     {
